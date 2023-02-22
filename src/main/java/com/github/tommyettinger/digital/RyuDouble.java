8380486aa40951b9fbc5712b4567cda4ad2430fe
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-02-21 23:09:21 -0800

    Give RyuDouble scientific and decimal modes.
    I'll follow up with RyuFloat soon.

--- src/main/java/com/github/tommyettinger/digital/RyuDouble.java
+++ src/main/java/com/github/tommyettinger/digital/RyuDouble.java
@@ -73,7 +73,7 @@ public final class RyuDouble {
     }
   }
 
-  public static String doubleToString(double value) {
+  public static String signed(double value) {
     // Step 1: Decode the floating point number, and unify normalized and subnormal cases.
     // First, handle all the trivial cases.
     if (Double.isNaN(value)) return "NaN";
@@ -300,6 +300,379 @@ public final class RyuDouble {
       }
       return new String(result, 0, index);
     }
+  }
+
+  public static String decimal(double value) {
+    // Step 1: Decode the floating point number, and unify normalized and subnormal cases.
+    // First, handle all the trivial cases.
+    if (Double.isNaN(value)) return "NaN";
+    if (value == Double.POSITIVE_INFINITY) return "Infinity";
+    if (value == Double.NEGATIVE_INFINITY) return "-Infinity";
+    long bits = Double.doubleToLongBits(value);
+    if (bits == 0) return "0.0";
+    if (bits == 0x8000000000000000L) return "-0.0";
+
+    // Otherwise extract the mantissa and exponent bits and run the full algorithm.
+    int ieeeExponent = (int) ((bits >>> DOUBLE_MANTISSA_BITS) & DOUBLE_EXPONENT_MASK);
+    long ieeeMantissa = bits & DOUBLE_MANTISSA_MASK;
+    int e2;
+    long m2;
+    if (ieeeExponent == 0) {
+      // Denormal number - no implicit leading 1, and the exponent is 1, not 0.
+      e2 = 1 - DOUBLE_EXPONENT_BIAS - DOUBLE_MANTISSA_BITS;
+      m2 = ieeeMantissa;
+    } else {
+      // Add implicit leading 1.
+      e2 = ieeeExponent - DOUBLE_EXPONENT_BIAS - DOUBLE_MANTISSA_BITS;
+      m2 = ieeeMantissa | (1L << DOUBLE_MANTISSA_BITS);
+    }
+
+    boolean sign = bits < 0;
+
+    // Step 2: Determine the interval of legal decimal representations.
+    boolean even = (m2 & 1) == 0;
+    final long mv = 4 * m2;
+    final long mp = 4 * m2 + 2;
+    final int mmShift = ((m2 != (1L << DOUBLE_MANTISSA_BITS)) || (ieeeExponent == 1)) ? 1 : 0;
+    final long mm = 4 * m2 - 1 - mmShift;
+    e2 -= 2;
+
+    // Step 3: Convert to a decimal power base using 128-bit arithmetic.
+    // -1077 = 1 - 1023 - 53 - 2 <= e_2 - 2 <= 2046 - 1023 - 53 - 2 = 968
+    long dv, dp, dm;
+    final int e10;
+    boolean dmIsTrailingZeros = false, dvIsTrailingZeros = false;
+    if (e2 >= 0) {
+      final int q = Math.max(0, ((e2 * 78913) >>> 18) - 1);
+      // k = constant + floor(log_2(5^q))
+      final int k = POW5_INV_BITCOUNT + pow5bits(q) - 1;
+      final int i = -e2 + q + k;
+      dv = mulPow5InvDivPow2(mv, q, i);
+      dp = mulPow5InvDivPow2(mp, q, i);
+      dm = mulPow5InvDivPow2(mm, q, i);
+      e10 = q;
+
+      if (q <= 21) {
+        if (mv % 5 == 0) {
+          dvIsTrailingZeros = multipleOfPowerOf5(mv, q);
+        } else if (even) {
+          dmIsTrailingZeros = multipleOfPowerOf5(mm, q);
+        } else if (multipleOfPowerOf5(mp, q)) {
+          dp--;
+        }
+      }
+    } else {
+      final int q = Math.max(0, ((-e2 * 732923) >>> 20) - 1);
+      final int i = -e2 - q;
+      final int k = pow5bits(i) - POW5_BITCOUNT;
+      final int j = q - k;
+      dv = mulPow5divPow2(mv, i, j);
+      dp = mulPow5divPow2(mp, i, j);
+      dm = mulPow5divPow2(mm, i, j);
+      e10 = q + e2;
+      if (q <= 1) {
+        dvIsTrailingZeros = true;
+        if (even) {
+          dmIsTrailingZeros = mmShift == 1;
+        } else {
+          dp--;
+        }
+      } else if (q < 63) {
+        dvIsTrailingZeros = (mv & ((1L << (q - 1)) - 1)) == 0;
+      }
+    }
+
+    // Step 4: Find the shortest decimal representation in the interval of legal representations.
+    //
+    // We do some extra work here in order to follow Float/Double.toString semantics. In particular,
+    // that requires printing in scientific format if and only if the exponent is between -3 and 7,
+    // and it requires printing at least two decimal digits.
+    //
+    // Above, we moved the decimal dot all the way to the right, so now we need to count digits to
+    // figure out the correct exponent for scientific notation.
+    final int vplength = decimalLength(dp);
+    int exp = e10 + vplength - 1;
+
+    // Double.toString semantics requires using scientific notation if and only if outside this range.
+    final boolean scientificNotation = false;
+
+    int removed = 0;
+
+    int lastRemovedDigit = 0;
+    long output;
+    if (dmIsTrailingZeros || dvIsTrailingZeros) {
+      while (dp / 10 > dm / 10) {
+        dmIsTrailingZeros &= dm % 10 == 0;
+        dvIsTrailingZeros &= lastRemovedDigit == 0;
+        lastRemovedDigit = (int) (dv % 10);
+        dp /= 10;
+        dv /= 10;
+        dm /= 10;
+        removed++;
+      }
+      if (dmIsTrailingZeros) {
+        while (dm % 10 == 0) {
+          dvIsTrailingZeros &= lastRemovedDigit == 0;
+          lastRemovedDigit = (int) (dv % 10);
+          dp /= 10;
+          dv /= 10;
+          dm /= 10;
+          removed++;
+        }
+      }
+      if (dvIsTrailingZeros && (lastRemovedDigit == 5) && (dv % 2 == 0)) {
+        // Round even if the exact numbers is .....50..0.
+        lastRemovedDigit = 4;
+      }
+      output = dv +
+          (dv == dm && !dmIsTrailingZeros || lastRemovedDigit >= 5 ? 1 : 0);
+    } else {
+      while (dp / 10 > dm / 10) {
+        lastRemovedDigit = (int) (dv % 10);
+        dp /= 10;
+        dv /= 10;
+        dm /= 10;
+        removed++;
+      }
+      output = dv + ((dv == dm || (lastRemovedDigit >= 5)) ? 1 : 0);
+    }
+    int olength = vplength - removed;
+
+    // Step 5: Print the decimal representation.
+    // We follow Double.toString semantics here.
+    char[] result = new char[24];
+    int index = 0;
+    if (sign) {
+      result[index++] = '-';
+    }
+
+    // Values in the interval [1E-3, 1E7) are special.
+    // Otherwise, follow the Java spec for values in the interval [1E-3, 1E7).
+    if (exp < 0) {
+      // Decimal dot is before any of the digits.
+      result[index++] = '0';
+      result[index++] = '.';
+      for (int i = -1; i > exp; i--) {
+        result[index++] = '0';
+      }
+      int current = index;
+      for (int i = 0; i < olength; i++) {
+        result[current + olength - i - 1] = (char) ('0' + output % 10);
+        output /= 10;
+        index++;
+      }
+    } else if (exp + 1 >= olength) {
+      // Decimal dot is after any of the digits.
+      for (int i = 0; i < olength; i++) {
+        result[index + olength - i - 1] = (char) ('0' + output % 10);
+        output /= 10;
+      }
+      index += olength;
+      for (int i = olength; i < exp + 1; i++) {
+        result[index++] = '0';
+      }
+      result[index++] = '.';
+      result[index++] = '0';
+    } else {
+      // Decimal dot is somewhere between the digits.
+      int current = index + 1;
+      for (int i = 0; i < olength; i++) {
+        if (olength - i - 1 == exp) {
+          result[current + olength - i - 1] = '.';
+          current--;
+        }
+        result[current + olength - i - 1] = (char) ('0' + output % 10);
+        output /= 10;
+      }
+      index += olength + 1;
+    }
+    return new String(result, 0, index);
+  }
+
+  public static String scientific(double value) {
+    // Step 1: Decode the floating point number, and unify normalized and subnormal cases.
+    // First, handle all the trivial cases.
+    if (Double.isNaN(value)) return "NaN";
+    if (value == Double.POSITIVE_INFINITY) return "Infinity";
+    if (value == Double.NEGATIVE_INFINITY) return "-Infinity";
+    long bits = Double.doubleToLongBits(value);
+    if (bits == 0) return "0.0E+00";
+    if (bits == 0x8000000000000000L) return "-0.0E+00";
+
+    // Otherwise extract the mantissa and exponent bits and run the full algorithm.
+    int ieeeExponent = (int) ((bits >>> DOUBLE_MANTISSA_BITS) & DOUBLE_EXPONENT_MASK);
+    long ieeeMantissa = bits & DOUBLE_MANTISSA_MASK;
+    int e2;
+    long m2;
+    if (ieeeExponent == 0) {
+      // Denormal number - no implicit leading 1, and the exponent is 1, not 0.
+      e2 = 1 - DOUBLE_EXPONENT_BIAS - DOUBLE_MANTISSA_BITS;
+      m2 = ieeeMantissa;
+    } else {
+      // Add implicit leading 1.
+      e2 = ieeeExponent - DOUBLE_EXPONENT_BIAS - DOUBLE_MANTISSA_BITS;
+      m2 = ieeeMantissa | (1L << DOUBLE_MANTISSA_BITS);
+    }
+
+    boolean sign = bits < 0;
+
+    // Step 2: Determine the interval of legal decimal representations.
+    boolean even = (m2 & 1) == 0;
+    final long mv = 4 * m2;
+    final long mp = 4 * m2 + 2;
+    final int mmShift = ((m2 != (1L << DOUBLE_MANTISSA_BITS)) || (ieeeExponent == 1)) ? 1 : 0;
+    final long mm = 4 * m2 - 1 - mmShift;
+    e2 -= 2;
+
+    // Step 3: Convert to a decimal power base using 128-bit arithmetic.
+    // -1077 = 1 - 1023 - 53 - 2 <= e_2 - 2 <= 2046 - 1023 - 53 - 2 = 968
+    long dv, dp, dm;
+    final int e10;
+    boolean dmIsTrailingZeros = false, dvIsTrailingZeros = false;
+    if (e2 >= 0) {
+      final int q = Math.max(0, ((e2 * 78913) >>> 18) - 1);
+      // k = constant + floor(log_2(5^q))
+      final int k = POW5_INV_BITCOUNT + pow5bits(q) - 1;
+      final int i = -e2 + q + k;
+      dv = mulPow5InvDivPow2(mv, q, i);
+      dp = mulPow5InvDivPow2(mp, q, i);
+      dm = mulPow5InvDivPow2(mm, q, i);
+      e10 = q;
+
+      if (q <= 21) {
+        if (mv % 5 == 0) {
+          dvIsTrailingZeros = multipleOfPowerOf5(mv, q);
+        } else if (even) {
+          dmIsTrailingZeros = multipleOfPowerOf5(mm, q);
+        } else if (multipleOfPowerOf5(mp, q)) {
+          dp--;
+        }
+      }
+    } else {
+      final int q = Math.max(0, ((-e2 * 732923) >>> 20) - 1);
+      final int i = -e2 - q;
+      final int k = pow5bits(i) - POW5_BITCOUNT;
+      final int j = q - k;
+      dv = mulPow5divPow2(mv, i, j);
+      dp = mulPow5divPow2(mp, i, j);
+      dm = mulPow5divPow2(mm, i, j);
+      e10 = q + e2;
+      if (q <= 1) {
+        dvIsTrailingZeros = true;
+        if (even) {
+          dmIsTrailingZeros = mmShift == 1;
+        } else {
+          dp--;
+        }
+      } else if (q < 63) {
+        dvIsTrailingZeros = (mv & ((1L << (q - 1)) - 1)) == 0;
+      }
+    }
+
+    // Step 4: Find the shortest decimal representation in the interval of legal representations.
+    //
+    // We do some extra work here in order to follow Float/Double.toString semantics. In particular,
+    // that requires printing in scientific format if and only if the exponent is between -3 and 7,
+    // and it requires printing at least two decimal digits.
+    //
+    // Above, we moved the decimal dot all the way to the right, so now we need to count digits to
+    // figure out the correct exponent for scientific notation.
+    final int vplength = decimalLength(dp);
+    int exp = e10 + vplength - 1;
+
+    // Double.toString semantics requires using scientific notation if and only if outside this range.
+    boolean scientificNotation = true;
+
+    int removed = 0;
+
+    int lastRemovedDigit = 0;
+    long output;
+    if (dmIsTrailingZeros || dvIsTrailingZeros) {
+      while (dp / 10 > dm / 10) {
+        if (dp < 100) {
+          // Double.toString semantics requires printing at least two digits.
+          break;
+        }
+        dmIsTrailingZeros &= dm % 10 == 0;
+        dvIsTrailingZeros &= lastRemovedDigit == 0;
+        lastRemovedDigit = (int) (dv % 10);
+        dp /= 10;
+        dv /= 10;
+        dm /= 10;
+        removed++;
+      }
+      if (dmIsTrailingZeros) {
+        while (dm % 10 == 0) {
+          if (dp < 100) {
+            // Double.toString semantics requires printing at least two digits.
+            break;
+          }
+          dvIsTrailingZeros &= lastRemovedDigit == 0;
+          lastRemovedDigit = (int) (dv % 10);
+          dp /= 10;
+          dv /= 10;
+          dm /= 10;
+          removed++;
+        }
+      }
+      if (dvIsTrailingZeros && (lastRemovedDigit == 5) && (dv % 2 == 0)) {
+        // Round even if the exact numbers is .....50..0.
+        lastRemovedDigit = 4;
+      }
+      output = dv +
+          (dv == dm && !dmIsTrailingZeros || lastRemovedDigit >= 5 ? 1 : 0);
+    } else {
+      while (dp / 10 > dm / 10) {
+        if (dp < 100) {
+          // Double.toString semantics requires printing at least two digits.
+          break;
+        }
+        lastRemovedDigit = (int) (dv % 10);
+        dp /= 10;
+        dv /= 10;
+        dm /= 10;
+        removed++;
+      }
+      output = dv + ((dv == dm || (lastRemovedDigit >= 5)) ? 1 : 0);
+    }
+    int olength = vplength - removed;
+
+    // Step 5: Print the decimal representation.
+    // We follow Double.toString semantics here.
+    char[] result = new char[24];
+    int index = 0;
+    if (sign) {
+      result[index++] = '-';
+    }
+
+    // Values in the interval [1E-3, 1E7) are special.
+    // Print in the format x.xxxxxE-yy.
+    for (int i = 0; i < olength - 1; i++) {
+      int c = (int) (output % 10); output /= 10;
+      result[index + olength - i] = (char) ('0' + c);
+    }
+    result[index] = (char) ('0' + output % 10);
+    result[index + 1] = '.';
+    index += olength + 1;
+    if (olength == 1) {
+      result[index++] = '0';
+    }
+
+    // Print 'E', the exponent sign, and the exponent, which has at most three digits.
+    result[index++] = 'E';
+    if (exp < 0) {
+      result[index++] = '-';
+      exp = -exp;
+    }
+    if (exp >= 100) {
+      result[index++] = (char) ('0' + exp / 100);
+      exp %= 100;
+      result[index++] = (char) ('0' + exp / 10);
+    } else if (exp >= 10) {
+      result[index++] = (char) ('0' + exp / 10);
+    }
+    result[index++] = (char) ('0' + exp % 10);
+    return new String(result, 0, index);
   }
 
   private static int pow5bits(int e) {
---
+++ src/test/java/com/github/tommyettinger/digital/RyuChecks.java
@@ -0,0 +1,25 @@
+package com.github.tommyettinger.digital;
+
+public class RyuChecks {
+    public static double nextExclusiveDouble (AlternateRandom random) {
+        final long bits = random.nextLong();
+        return BitConversion.longBitsToDouble(1022L - Long.numberOfTrailingZeros(bits) << 52 | bits >>> 12);
+    }
+
+    /**
+     * Some interesting results:
+     * It should be obvious when you run this that Ryu prints a lot more digits. I'm not sure why this is.
+     * The decimal number Ryu prints as {@code 0.00039954273149628435} gets printed differently by Java depending on
+     * whether it is using {@code %g} or {@code %f}, with {@code 0.000399543} for the former and {@code 0.000400} for
+     * the latter.
+     * @param args unused
+     */
+    public static void main(String[] args) {
+        AlternateRandom random = new AlternateRandom(1234567890L);
+        for (int i = 0; i < 100; i++) {
+            double d = (nextExclusiveDouble(random) / nextExclusiveDouble(random)) * (nextExclusiveDouble(random) - 0.5f);
+            System.out.printf("Java general: %-20g, Java decimal: %-20f, Java scientific: %-20E\n", d, d, d);
+            System.out.printf("Ryu general : %-20s, Ryu decimal : %-20s, Ryu scientific : %-20s\n", RyuDouble.signed(d), RyuDouble.decimal(d), RyuDouble.scientific(d));
+        }
+    }
+}
