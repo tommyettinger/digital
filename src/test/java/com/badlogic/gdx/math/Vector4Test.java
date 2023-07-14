fe317f544d3bb5a4de84c431e9ceab735ce9ae91
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-07-13 21:37:35 -0700

    Couldn't reproduce the bug, but I have a test!

--- src/test/java/com/badlogic/gdx/math/Vector4Test.java
+++ src/test/java/com/badlogic/gdx/math/Vector4Test.java
@@ -3,7 +3,7 @@ package com.badlogic.gdx.math;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class Vector4Test {
 	@Test
@@ -14,5 +14,18 @@ public class Vector4Test {
 	@Test
 	public void testFromString () {
 		assertEquals(new Vector4(-5f, 42.00055f, 44444.32f, -1.975f), new Vector4().fromString("(-5,42.00055,44444.32,-1.9750)"));
+	}
+
+	@Test
+	public void testOnLine () {
+		Vector4 a = new Vector4(2, 1, 0, 0), b = new Vector4(-1, -0.5f, 0, 0);
+		assertTrue(a.isOnLine(b));
+		assertTrue(b.isOnLine(a));
+		a.w = -Float.MAX_VALUE;
+		assertFalse(a.isOnLine(b));
+		assertFalse(b.isOnLine(a));
+		b.w = Float.MAX_VALUE * 0.5f;
+		assertTrue(a.isOnLine(b));
+		assertTrue(b.isOnLine(a));
 	}
 }
