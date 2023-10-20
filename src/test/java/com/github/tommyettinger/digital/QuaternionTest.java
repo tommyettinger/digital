2b4bc3b1f4725b9df50761c6180db00b67e1c6c7
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-10-19 21:44:29 -0700

    Trying different libGDX versions, rotations.

--- src/test/java/com/github/tommyettinger/digital/QuaternionTest.java
+++ src/test/java/com/github/tommyettinger/digital/QuaternionTest.java
@@ -112,6 +112,8 @@ public class QuaternionTest {
 	}
 
 	/**
+	 * With 360 rotations:
+	 * <br>
 	 * On libGDX 1.9.11:
 	 * <pre>
 	 * 6.1743784 vs. 179.99883
@@ -124,19 +126,76 @@ public class QuaternionTest {
 	 * 10.768361 vs. 84.00223
 	 * 22.487045 vs. -179.9974
 	 * </pre>
+	 * <br>
+	 * With 371 rotations:
+	 * <br>
+	 * On libGDX 1.9.11:
+	 * <pre>
+	 * 45.835953 vs. 43.998928
+	 * -34.185814 vs. 88.00129
+	 * -145.0157 vs. 175.99911
+	 * </pre>
+	 * On libGDX 1.9.12:
+	 * <pre>
+	 * 45.835953 vs. 44.004166
+	 * -34.185814 vs. 87.99797
+	 * -145.0157 vs. 176.00278
+	 * </pre>
+	 * <br>
+	 * With 1 rotation:
+	 * <br>
+	 * On libGDX 1.9.11:
+	 * <pre>
+	 * 4.0000935 vs. 4.0001884
+	 * 7.9999995 vs. 7.9999976
+	 * 16.000097 vs. 16.00019
+	 * </pre>
+	 * On libGDX 1.9.12:
+	 * <pre>
+	 * 4.0000935 vs. 4.0000935
+	 * 7.9999995 vs. 7.9999995
+	 * 16.000097 vs. 16.000097
+	 * </pre>
+	 * <br>
+	 * With 3 rotations:
+	 * <br>
+	 * On libGDX 1.9.11:
+	 * <pre>
+	 * 18.336832 vs. 12.000094
+	 * 18.951181 vs. 23.999992
+	 * 51.003944 vs. 48.000126
+	 * </pre>
+	 * On libGDX 1.9.12:
+	 * <pre>
+	 * 18.336832 vs. 11.999998
+	 * 18.951181 vs. 24.000002
+	 * 51.003944 vs. 48.00004
+	 * </pre>
 	 */
 	@Test
 	public void testWhat() {
 		Quaternion r = new Quaternion();
 		Quaternion q = new Quaternion();
 		Quaternion rot = new Quaternion().setEulerAngles(4f, 8f, 16f);
-		for (int i = 0; i < 360; i++) {
+		System.out.println("With 1: ");
+		for (int i = 0; i < 1; i++) {
 			r.mulLeft(rot);
 			q.setEulerAngles(q.getYaw()+4f, q.getPitch()+8f, q.getRoll()+16f);
 		}
 		System.out.println(r.getYaw() + " vs. " + q.getYaw());
 		System.out.println(r.getPitch() + " vs. " + q.getPitch());
 		System.out.println(r.getRoll() + " vs. " + q.getRoll());
+		r.idt();
+		q.idt();
+		System.out.println("With 3: ");
+		for (int i = 0; i < 3; i++) {
+			r.mulLeft(rot);
+			q.setEulerAngles(q.getYaw()+4f, q.getPitch()+8f, q.getRoll()+16f);
+		}
+		System.out.println(r.getYaw() + " vs. " + q.getYaw());
+		System.out.println(r.getPitch() + " vs. " + q.getPitch());
+		System.out.println(r.getRoll() + " vs. " + q.getRoll());
+
 	}
 
 }
