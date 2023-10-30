b30c83fc38255af78ca8953c2b1dafb8c182cd5a
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-10-30 16:08:35 -0700

    Tried some things with COS_TO_SIN, nope.
    I'm not sure exactly what it might need.

--- src/test/java/com/github/tommyettinger/digital/CosTools.java
+++ src/test/java/com/github/tommyettinger/digital/CosTools.java
@@ -971,6 +971,16 @@ public final class CosTools {
         final int masked = floor & TABLE_MASK;
         final float from = COS_TABLE[masked], to = COS_TABLE[masked+1];
         return from + (to - from) * (radians - floor);
+        
+//        //Mean absolute error:     0.6366196871
+//        //Mean relative error:     1.0000005960
+//        //Maximum abs. error:      2.0000000000
+//        //Maximum rel. error:      2.4425818920
+//        radians = Math.abs(radians) * radToIndex;
+//        final int floor = (int)radians;
+//        final int masked = floor + COS_TO_SIN & TABLE_MASK;
+//        final float from = COS_TABLE[masked], to = COS_TABLE[masked+1];
+//        return from + (to - from) * (radians - floor);
     }
 
     /**
