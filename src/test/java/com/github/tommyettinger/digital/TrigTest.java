3189dbd05ce707b99f9cf37b4febc40bea5650c3
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-10-31 18:56:57 -0700

    The two-table approach does seem cleaner.

--- src/test/java/com/github/tommyettinger/digital/TrigTest.java
+++ src/test/java/com/github/tommyettinger/digital/TrigTest.java
@@ -2,16 +2,21 @@ package com.github.tommyettinger.digital;
 
 import org.junit.Test;
 
+import static com.github.tommyettinger.digital.TrigTools.*;
+
 public class TrigTest {
     @Test
     public void testTrigVersusCos(){
         for (int i = -9; i < 10; i++) {
+            System.out.printf("Math %d:          , sin %.10f, cos %.10f\n", i,
+                    Math.sin(i),
+                    Math.cos(i));
             System.out.printf("Trig %d: idx %05d, sin %.10f, cos %.10f\n", i, TrigTools.radiansToTableIndex(i),
-                    TrigTools.SIN_TABLE[TrigTools.radiansToTableIndex(i)],
-                    TrigTools.SIN_TABLE[TrigTools.radiansToTableIndex(i) + TrigTools.SIN_TO_COS & TrigTools.TABLE_MASK]);
+                    SIN_TABLE[TrigTools.radiansToTableIndex(i)],
+                    SIN_TABLE[TrigTools.radiansToTableIndex(i) + TrigTools.SIN_TO_COS & TABLE_MASK]);
             System.out.printf("Cos  %d: idx %05d, sin %.10f, cos %.10f\n", i, CosTools.radiansToTableIndex(i),
-                    CosTools.COS_TABLE[CosTools.radiansToTableIndex(i) + CosTools.COS_TO_SIN & CosTools.TABLE_MASK],
-                    CosTools.COS_TABLE[CosTools.radiansToTableIndex(i)]);
+                    SIN_TABLE[TrigTools.radiansToTableIndex(i)],
+                    CosTools.COS_TABLE[TrigTools.radiansToTableIndex(i)]);
         }
     }
 }
