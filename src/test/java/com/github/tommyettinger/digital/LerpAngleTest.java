c32c1d65033d92a67860b317728c8b8147e8cb63
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-11-19 20:39:43 -0800

    I can't find any easy way to simplify this.
    The original lerpAngleDeg() is probably best.

--- src/test/java/com/github/tommyettinger/digital/LerpAngleTest.java
+++ src/test/java/com/github/tommyettinger/digital/LerpAngleTest.java
@@ -51,4 +51,35 @@ public class LerpAngleTest {
             }
         }
     }
+
+    /**
+     * Linearly interpolates between two angles in degrees. Takes into account that angles wrap at 360 degrees and
+     * always takes the direction with the smallest delta angle.
+     *
+     * @param fromDegrees start angle in degrees
+     * @param toDegrees   target angle in degrees
+     * @param progress    interpolation value in the range [0, 1]
+     * @return the interpolated angle in the range [0, 360)
+     */
+    public static float lerpAngleDegAlt(float fromDegrees, float toDegrees, float progress) {
+//        float delta = ((toDegrees - fromDegrees)) % 360f;
+//        return ((fromDegrees + delta * progress) % 360f + 360f) % 360f;
+
+        float d = toDegrees - fromDegrees;
+        d = fromDegrees + progress * ((d % 360f + 360f + 180f) % 360f - 180f);
+        return (d % 360f + 360f) % 360f;
+    }
+
+    @Test
+    public void testDegreesAltFloat() {
+        for (float a = 0.05f; a <= 1f; a += 0.123f) {
+            for (float lb : left) {
+                float l = lb * 360;
+                for (float rb : right) {
+                    float r = rb * 360;
+                    Assert.assertEquals("l: " + l + ", r: " + r + ", a: " + a, MathUtils.lerpAngleDeg(l, r, a), lerpAngleDegAlt(l, r, a), 0.01f);
+                }
+            }
+        }
+    }
 }
