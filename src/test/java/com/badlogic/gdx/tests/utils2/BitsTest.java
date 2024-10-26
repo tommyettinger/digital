e3a66f52df82525a01accd4dbe6449d68f1fd911
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-10-26 15:32:25 -0700

    I guess nobody uses Bits...
    Bits.hashCode() crashes with an ArrayIndexOutOfBoundsException on various inputs; a test here replicates this. There's also a fix here in utils2.

---
+++ src/test/java/com/badlogic/gdx/tests/utils2/BitsTest.java
@@ -0,0 +1,19 @@
+package com.badlogic.gdx.tests.utils2;
+
+import com.badlogic.gdx.utils2.Bits;
+import org.junit.Test;
+
+public class BitsTest {
+    @Test
+    public void testHashCode(){
+        Bits bits = new Bits(1);
+        int empty = bits.hashCode();
+        bits.set(63);
+        int one = bits.hashCode();
+        System.out.println(one);
+        for (int i = 62; i >= 0; i--) {
+            bits.set(i);
+            System.out.println(bits.hashCode());
+        }
+    }
+}
