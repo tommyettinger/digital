67a340cbf38028c242e86277b01bd0ee269c2068
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-01-26 21:35:54 -0800

    StringF may work now?
    Needs tests.

--- src/main/java/com/github/tommyettinger/digital/Stringf.java
+++ src/main/java/com/github/tommyettinger/digital/Stringf.java
@@ -21,8 +21,8 @@ package com.github.tommyettinger.digital;
  * A limited polyfill for {@code String.format()} where the platform doesn't provide it, such as on GWT or TeaVM.
  * This supports {@link #format(String, Object...)} with only some of the formatting options in the actual, much-larger
  * Formatter class: {@code %% , %s , %d , %f , %x , %X}, and some precision options, such as {@code %.3f} . Scientific
- * notation is not yet supported, though it could be added in. Some features will probably never be added here, like
- * time and date formatting.
+ * notation is supported, but not with specified precision, yet. Some features will probably never be added here, like
+ * time and date formatting, because this is GWT-compatible and GWT is missing lots of time-related code.
  * <br>
  * Some useful features here are that {@link #appendf(StringBuilder, String, Object...)} allows reusing a StringBuilder,
  * {@link #printf(String, Object...)} acts like a method on OutputStream (which is typically not provided if
@@ -51,7 +51,8 @@ public final class Stringf {
      * @return {@code sb}, after modifications
      */
     public static StringBuilder appendf(StringBuilder sb, String fmt, Object... args) {
-        int len = fmt.length(), arg = 0;
+        final int len = fmt.length();
+        int arg = 0;
         for (int i = 0; i < len; i++) {
             char curr = fmt.charAt(i);
             if(curr == '%') {
@@ -69,6 +70,22 @@ public final class Stringf {
                     // float
                     double d = ((Number) args[arg++]).doubleValue();
                     Base.BASE10.appendDecimal(sb, d, d < 0.0 ? 6 : 5);
+                } else if (curr == 'e') {
+                    // general, lower case
+                    double d = ((Number) args[arg++]).doubleValue();
+                    Base.BASE10.appendScientific(sb, d, false);
+                } else if (curr == 'E') {
+                    // general, lower case
+                    double d = ((Number) args[arg++]).doubleValue();
+                    Base.BASE10.appendScientific(sb, d, true);
+                } else if (curr == 'g') {
+                    // general, lower case
+                    double d = ((Number) args[arg++]).doubleValue();
+                    Base.BASE10.appendGeneral(sb, d, false);
+                } else if (curr == 'G') {
+                    // general, lower case
+                    double d = ((Number) args[arg++]).doubleValue();
+                    Base.BASE10.appendGeneral(sb, d, true);
                 } else if (curr == 'x') {
                     // digit, hex (uncapitalized)
                     sb.append(Base.BASE16.signed(((Number) args[arg++]).longValue()).toLowerCase());
@@ -101,15 +118,29 @@ public final class Stringf {
                     curr = fmt.charAt(++i);
                     if(curr == 'f'){
                         Base.BASE10.appendDecimal(sb, ((Number) args[arg++]).doubleValue());
+                    } else if(curr == 'e'){
+                        Base.BASE10.appendScientific(sb, ((Number) args[arg++]).doubleValue(), false);
+                    } else if(curr == 'E'){
+                        Base.BASE10.appendScientific(sb, ((Number) args[arg++]).doubleValue(), true);
+                    } else if(curr == 'g'){
+                        Base.BASE10.appendGeneral(sb, ((Number) args[arg++]).doubleValue(), false);
+                    } else if(curr == 'G'){
+                        Base.BASE10.appendGeneral(sb, ((Number) args[arg++]).doubleValue(), true);
                     }
                     else {
                         int precision = Base.BASE10.readInt(fmt, i, len);
-                        while ((curr = fmt.charAt(++i)) >= '0' && curr <= '9'){
+                        while ((curr = fmt.charAt(++i)) >= '0' && curr <= '9') {
                         }
-                        if(curr == 'f')
-                        {
-                            double d = ((Number) args[arg++]).doubleValue();
-                            Base.BASE10.appendDecimal(sb, d, -10000, precision);
+                        if (curr == 'f') {
+                            Base.BASE10.appendDecimal(sb, ((Number) args[arg++]).doubleValue(), -10000, precision);
+                        } else if (curr == 'e') {
+                            Base.BASE10.appendScientific(sb, ((Number) args[arg++]).doubleValue(), false);
+                        } else if (curr == 'E') {
+                            Base.BASE10.appendScientific(sb, ((Number) args[arg++]).doubleValue(), true);
+                        } else if (curr == 'g') {
+                            Base.BASE10.appendGeneral(sb, ((Number) args[arg++]).doubleValue(), false);
+                        } else if (curr == 'G') {
+                            Base.BASE10.appendGeneral(sb, ((Number) args[arg++]).doubleValue(), true);
                         }
                     }
                 } else if (curr >= '1' && curr <= '9') {
