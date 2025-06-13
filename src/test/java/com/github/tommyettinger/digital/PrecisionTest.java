6b73cac404cdebe94762fd9e2fa647678a5f6aab
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-06-12 20:36:43 -0700

    Our best sine and cosine by precision so far.
    These are as fast as I think I can get the Jolt approximations; they aren't as fast as the "smoother" ones we already have, but are so precise that they're at most 2 ULPs off of what Math.sin/cos gets.

--- src/test/java/com/github/tommyettinger/digital/PrecisionTest.java
+++ src/test/java/com/github/tommyettinger/digital/PrecisionTest.java
@@ -3662,13 +3662,11 @@ CONST f32x2 sincos(s16 int_angle) {
     public static float sinJolt(float angle) {
         // Implementation based on sinf.c from the cephes library, combines sinf and cosf in a single function, changes octants to quadrants and vectorizes it
         // Original implementation by Stephen L. Moshier (See: http://www.moshier.net/)
-        int bits = BitConversion.floatToIntBits(angle);
-        int sinSign = bits & 0x80000000;
-        float x = BitConversion.intBitsToFloat(bits ^ sinSign);
+        float x = Math.abs(angle);
         int quadrant = (int)(0.6366197723675814 * x + 0.5);
         x = ((x - quadrant * 1.5703125f) - quadrant * 0.0004837512969970703125f) - quadrant * 7.549789948768648e-8f;
         float x2 = x * x, s;
-        switch ((sinSign >>> 30 ^ quadrant) & 3) {
+        switch (quadrant & 3) {
             case 0:
                 s = ((-1.9515295891e-4f * x2 + 8.3321608736e-3f) * x2 - 1.6666654611e-1f) * x2 * x + x;
                 break;
@@ -3681,7 +3679,7 @@ CONST f32x2 sincos(s16 int_angle) {
             default:
                 s = (((-2.443315711809948e-5f * x2 + 1.388731625493765e-3f) * x2 - 4.166664568298827e-2f) * x2 * x2 + 0.5f * x2 - 1f);
         }
-        return s;
+        return s * Math.signum(angle);
     }
 
     public static float cosJolt(float angle) {
@@ -3690,6 +3688,98 @@ CONST f32x2 sincos(s16 int_angle) {
         float x = Math.abs(angle);
         int quadrant = (int)(0.6366197723675814f * x + 0.5);
         x = ((x - quadrant * 1.5703125f) - quadrant * 0.0004837512969970703125f) - quadrant * 7.549789948768648e-8f;
+        float x2 = x * x, s;
+        switch (quadrant & 3) {
+            case 3:
+                s = ((-1.9515295891e-4f * x2 + 8.3321608736e-3f) * x2 - 1.6666654611e-1f) * x2 * x + x;
+                break;
+            case 0:
+                s = ((2.443315711809948e-5f * x2 - (1.388731625493765e-3f)) * x2 + (4.166664568298827e-2f)) * x2 * x2 - 0.5f * x2 + 1f;
+                break;
+            case 1:
+                s = (((1.9515295891e-4f * x2 - 8.3321608736e-3f) * x2 + 1.6666654611e-1f) * x2 * x - x);
+                break;
+            default:
+                s = (((-2.443315711809948e-5f * x2 + 1.388731625493765e-3f) * x2 - 4.166664568298827e-2f) * x2 * x2 + 0.5f * x2 - 1f);
+        }
+        return s;
+    }
+
+    public static float sinDegJolt(float angle) {
+        // Implementation based on sinf.c from the cephes library, combines sinf and cosf in a single function, changes octants to quadrants and vectorizes it
+        // Original implementation by Stephen L. Moshier (See: http://www.moshier.net/)
+        float x = Math.abs(angle);
+        int quadrant = (int)(0.011111111f * x + 0.5f);
+        x = (x - quadrant * 90f) * (PI2 / 360f);
+        float x2 = x * x, s;
+        switch (quadrant & 3) {
+            case 0:
+                s = ((-1.9515295891e-4f * x2 + 8.3321608736e-3f) * x2 - 1.6666654611e-1f) * x2 * x + x;
+                break;
+            case 1:
+                s = ((2.443315711809948e-5f * x2 - (1.388731625493765e-3f)) * x2 + (4.166664568298827e-2f)) * x2 * x2 - 0.5f * x2 + 1f;
+                break;
+            case 2:
+                s = (((1.9515295891e-4f * x2 - 8.3321608736e-3f) * x2 + 1.6666654611e-1f) * x2 * x - x);
+                break;
+            default:
+                s = (((-2.443315711809948e-5f * x2 + 1.388731625493765e-3f) * x2 - 4.166664568298827e-2f) * x2 * x2 + 0.5f * x2 - 1f);
+        }
+        return s * Math.signum(angle);
+    }
+
+    public static float cosDegJolt(float angle) {
+        // Implementation based on sinf.c from the cephes library, combines sinf and cosf in a single function, changes octants to quadrants and vectorizes it
+        // Original implementation by Stephen L. Moshier (See: http://www.moshier.net/)
+        float x = Math.abs(angle);
+        int quadrant = (int)(0.011111111f * x + 0.5f);
+        x = (x - quadrant * 90f) * (PI2 / 360f);
+        float x2 = x * x, s;
+        switch (quadrant & 3) {
+            case 3:
+                s = ((-1.9515295891e-4f * x2 + 8.3321608736e-3f) * x2 - 1.6666654611e-1f) * x2 * x + x;
+                break;
+            case 0:
+                s = ((2.443315711809948e-5f * x2 - (1.388731625493765e-3f)) * x2 + (4.166664568298827e-2f)) * x2 * x2 - 0.5f * x2 + 1f;
+                break;
+            case 1:
+                s = (((1.9515295891e-4f * x2 - 8.3321608736e-3f) * x2 + 1.6666654611e-1f) * x2 * x - x);
+                break;
+            default:
+                s = (((-2.443315711809948e-5f * x2 + 1.388731625493765e-3f) * x2 - 4.166664568298827e-2f) * x2 * x2 + 0.5f * x2 - 1f);
+        }
+        return s;
+    }
+
+    public static float sinTurnsJolt(float angle) {
+        // Implementation based on sinf.c from the cephes library, combines sinf and cosf in a single function, changes octants to quadrants and vectorizes it
+        // Original implementation by Stephen L. Moshier (See: http://www.moshier.net/)
+        float x = Math.abs(angle);
+        int quadrant = (int)(4 * x + 0.5f);
+        x = (x - quadrant * 0.25f) * PI2;
+        float x2 = x * x, s;
+        switch (quadrant & 3) {
+            case 0:
+                s = ((-1.9515295891e-4f * x2 + 8.3321608736e-3f) * x2 - 1.6666654611e-1f) * x2 * x + x;
+                break;
+            case 1:
+                s = ((2.443315711809948e-5f * x2 - (1.388731625493765e-3f)) * x2 + (4.166664568298827e-2f)) * x2 * x2 - 0.5f * x2 + 1f;
+                break;
+            case 2:
+                s = (((1.9515295891e-4f * x2 - 8.3321608736e-3f) * x2 + 1.6666654611e-1f) * x2 * x - x);
+                break;
+            default:
+                s = (((-2.443315711809948e-5f * x2 + 1.388731625493765e-3f) * x2 - 4.166664568298827e-2f) * x2 * x2 + 0.5f * x2 - 1f);
+        }
+        return s * Math.signum(angle);
+    }
+
+    public static float cosTurnsJolt(float angle) {
+        // Implementation based on sinf.c from the cephes library, combines sinf and cosf in a single function, changes octants to quadrants and vectorizes it
+        // Original implementation by Stephen L. Moshier (See: http://www.moshier.net/)
+        float x = Math.abs(angle);
+        int quadrant = (int)(4 * x + 0.5f);
+        x = (x - quadrant * 0.25f) * PI2;
         float x2 = x * x, s;
         switch (quadrant & 3) {
             case 3:
@@ -3768,103 +3858,6 @@ CONST f32x2 sincos(s16 int_angle) {
 //        outSin = Vec4::sXor(s, sin_sign.ReinterpretAsFloat());
 //        outCos = Vec4::sXor(c, cos_sign.ReinterpretAsFloat());
 //    }
-
-
-    public static float sinDegJolt(float angle) {
-        // Implementation based on sinf.c from the cephes library, combines sinf and cosf in a single function, changes octants to quadrants and vectorizes it
-        // Original implementation by Stephen L. Moshier (See: http://www.moshier.net/)
-        int bits = BitConversion.floatToIntBits(angle);
-        int sinSign = bits & 0x80000000;
-        float x = BitConversion.intBitsToFloat(bits ^ sinSign);
-        int quadrant = (int)(0.011111111f * x + 0.5f);
-        x = (x - quadrant * 90f) * (PI2 / 360f);
-        float x2 = x * x, s;
-        switch ((sinSign >>> 30 ^ quadrant) & 3) {
-            case 0:
-                s = ((-1.9515295891e-4f * x2 + 8.3321608736e-3f) * x2 - 1.6666654611e-1f) * x2 * x + x;
-                break;
-            case 1:
-                s = ((2.443315711809948e-5f * x2 - (1.388731625493765e-3f)) * x2 + (4.166664568298827e-2f)) * x2 * x2 - 0.5f * x2 + 1f;
-                break;
-            case 2:
-                s = (((1.9515295891e-4f * x2 - 8.3321608736e-3f) * x2 + 1.6666654611e-1f) * x2 * x - x);
-                break;
-            default:
-                s = (((-2.443315711809948e-5f * x2 + 1.388731625493765e-3f) * x2 - 4.166664568298827e-2f) * x2 * x2 + 0.5f * x2 - 1f);
-        }
-        return s;
-    }
-
-    public static float cosDegJolt(float angle) {
-        // Implementation based on sinf.c from the cephes library, combines sinf and cosf in a single function, changes octants to quadrants and vectorizes it
-        // Original implementation by Stephen L. Moshier (See: http://www.moshier.net/)
-        float x = Math.abs(angle);
-        int quadrant = (int)(0.011111111f * x + 0.5f);
-        x = (x - quadrant * 90f) * (PI2 / 360f);
-        float x2 = x * x, s;
-        switch (quadrant & 3) {
-            case 3:
-                s = ((-1.9515295891e-4f * x2 + 8.3321608736e-3f) * x2 - 1.6666654611e-1f) * x2 * x + x;
-                break;
-            case 0:
-                s = ((2.443315711809948e-5f * x2 - (1.388731625493765e-3f)) * x2 + (4.166664568298827e-2f)) * x2 * x2 - 0.5f * x2 + 1f;
-                break;
-            case 1:
-                s = (((1.9515295891e-4f * x2 - 8.3321608736e-3f) * x2 + 1.6666654611e-1f) * x2 * x - x);
-                break;
-            default:
-                s = (((-2.443315711809948e-5f * x2 + 1.388731625493765e-3f) * x2 - 4.166664568298827e-2f) * x2 * x2 + 0.5f * x2 - 1f);
-        }
-        return s;
-    }
-
-    public static float sinTurnsJolt(float angle) {
-        // Implementation based on sinf.c from the cephes library, combines sinf and cosf in a single function, changes octants to quadrants and vectorizes it
-        // Original implementation by Stephen L. Moshier (See: http://www.moshier.net/)
-        int bits = BitConversion.floatToIntBits(angle);
-        int sinSign = bits & 0x80000000;
-        float x = BitConversion.intBitsToFloat(bits ^ sinSign);
-        int quadrant = (int)(4 * x + 0.5f);
-        x = (x - quadrant * 0.25f) * PI2;
-        float x2 = x * x, s;
-        switch ((sinSign >>> 30 ^ quadrant) & 3) {
-            case 0:
-                s = ((-1.9515295891e-4f * x2 + 8.3321608736e-3f) * x2 - 1.6666654611e-1f) * x2 * x + x;
-                break;
-            case 1:
-                s = ((2.443315711809948e-5f * x2 - (1.388731625493765e-3f)) * x2 + (4.166664568298827e-2f)) * x2 * x2 - 0.5f * x2 + 1f;
-                break;
-            case 2:
-                s = (((1.9515295891e-4f * x2 - 8.3321608736e-3f) * x2 + 1.6666654611e-1f) * x2 * x - x);
-                break;
-            default:
-                s = (((-2.443315711809948e-5f * x2 + 1.388731625493765e-3f) * x2 - 4.166664568298827e-2f) * x2 * x2 + 0.5f * x2 - 1f);
-        }
-        return s;
-    }
-
-    public static float cosTurnsJolt(float angle) {
-        // Implementation based on sinf.c from the cephes library, combines sinf and cosf in a single function, changes octants to quadrants and vectorizes it
-        // Original implementation by Stephen L. Moshier (See: http://www.moshier.net/)
-        float x = Math.abs(angle);
-        int quadrant = (int)(4 * x + 0.5f);
-        x = (x - quadrant * 0.25f) * PI2;
-        float x2 = x * x, s;
-        switch (quadrant & 3) {
-            case 3:
-                s = ((-1.9515295891e-4f * x2 + 8.3321608736e-3f) * x2 - 1.6666654611e-1f) * x2 * x + x;
-                break;
-            case 0:
-                s = ((2.443315711809948e-5f * x2 - (1.388731625493765e-3f)) * x2 + (4.166664568298827e-2f)) * x2 * x2 - 0.5f * x2 + 1f;
-                break;
-            case 1:
-                s = (((1.9515295891e-4f * x2 - 8.3321608736e-3f) * x2 + 1.6666654611e-1f) * x2 * x - x);
-                break;
-            default:
-                s = (((-2.443315711809948e-5f * x2 + 1.388731625493765e-3f) * x2 - 4.166664568298827e-2f) * x2 * x2 + 0.5f * x2 - 1f);
-        }
-        return s;
-    }
 
 
     /**
