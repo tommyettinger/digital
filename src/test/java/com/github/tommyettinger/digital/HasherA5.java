fbb077dd6087bbb601956add2103c25dd4f86994
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-02-26 19:18:57 -0800

    CharSequence and String.

--- src/test/java/com/github/tommyettinger/digital/HasherA5.java
+++ src/test/java/com/github/tommyettinger/digital/HasherA5.java
@@ -588,4 +588,220 @@ public final class HasherA5 {
 
         return a ^ b;
     }
+
+    /**
+     * A hashing function that is meant for smaller input CharSequences.
+     *
+     * @param data input CharSequence
+     * @return the 32-bit hash of data
+     */
+    public int hash(final CharSequence data) {
+        if (data == null) return 0;
+        return hash(data, 0, data.length());
+    }
+
+    /**
+     * A hashing function that is meant for smaller input CharSequences.
+     *
+     * @param data   input CharSequence
+     * @param start  starting index in data
+     * @param length how many items to use from data
+     * @return the 32-bit hash of data
+     */
+    public int hash(final CharSequence data, int start, int length) {
+        if (data == null || start < 0 || length < 0 || start >= data.length())
+            return 0;
+        int len = Math.min(length, data.length() - start);
+        int val01 = VAL01;
+        int val10 = VAL10;
+
+        int seed1 = 0x243F6A88 ^ len;
+        int seed2 = 0x85A308D3 ^ len;
+        int seed3 = 0xFB0BD3EA;
+        int seed4 = 0x0F58FD47;
+        int a, b, c, d;
+        long p;
+
+        p = (seed2 ^ (seed & 0xFFFFFFFFL)) * (seed1 ^ (seed >>> 32));
+        seed1 = (int) p;
+        seed2 = (int) (p >>> 32);
+        p = (seed3 ^ (seed & 0xFFFFFFFFL)) * (seed4 ^ (seed >>> 32));
+        seed3 = (int) p;
+        seed4 = (int) (p >>> 32);
+
+        int i = start;
+        if (len <= 4) {
+            if (len == 0) {
+                a = 0;
+                b = 0;
+            } else {
+                int last = i + len - 1;
+                a = data.charAt(i);
+                b = data.charAt(last);
+                if (len > 2) {
+                    int mo = len >>> 1;
+                    c = data.charAt(i + mo);
+                    d = data.charAt(last - mo);
+                    p = ((long) seed3 + c) * ((long) seed4 + d);
+                    seed3 = (int) p;
+                    seed4 = (int) (p >>> 32);
+                }
+            }
+        } else {
+            val01 ^= seed1;
+            val10 ^= seed2;
+
+            do {
+                final int s1 = seed1;
+                final int s4 = seed4;
+
+                p = ((long) seed1 + data.charAt(i)) * ((long) seed2 + data.charAt(i + 1));
+                seed1 = (int) p;
+                seed2 = (int) (p >>> 32);
+                p = ((long) seed3 + data.charAt(i + 2)) * ((long) seed4 + data.charAt(i + 3));
+                seed3 = (int) p;
+                seed4 = (int) (p >>> 32);
+
+                len -= 4;
+                i += 4;
+
+                seed1 += val01;
+                seed2 += s4;
+                seed3 += s1;
+                seed4 += val10;
+            } while (len > 4);
+
+            a = data.charAt(i + len - 2);
+            b = data.charAt(i + len - 1);
+
+            if (len > 2) {
+                c = data.charAt(i + len - 4);
+                d = data.charAt(i + len - 3);
+
+                p = ((long) seed3 + c) * ((long) seed4 + d);
+                seed3 = (int) p;
+                seed4 = (int) (p >>> 32);
+            }
+        }
+        seed1 ^= seed3;
+        seed2 ^= seed4;
+
+        p = ((long) seed1 + a) * ((long) seed2 + b);
+        seed1 = (int) p;
+        seed2 = (int) (p >>> 32);
+
+        p = ((long) seed1 ^ val01) * ((long) seed2);
+        a = (int) p;
+        b = (int) (p >>> 32);
+
+        return a ^ b;
+    }
+
+    /**
+     * A hashing function that is meant for smaller input Strings.
+     *
+     * @param data input String
+     * @return the 32-bit hash of data
+     */
+    public int hash(final String data) {
+        if (data == null) return 0;
+        return hash(data, 0, data.length());
+    }
+
+    /**
+     * A hashing function that is meant for smaller input Strings.
+     *
+     * @param data   input String
+     * @param start  starting index in data
+     * @param length how many items to use from data
+     * @return the 32-bit hash of data
+     */
+    public int hash(final String data, int start, int length) {
+        if (data == null || start < 0 || length < 0 || start >= data.length())
+            return 0;
+        int len = Math.min(length, data.length() - start);
+        int val01 = VAL01;
+        int val10 = VAL10;
+
+        int seed1 = 0x243F6A88 ^ len;
+        int seed2 = 0x85A308D3 ^ len;
+        int seed3 = 0xFB0BD3EA;
+        int seed4 = 0x0F58FD47;
+        int a, b, c, d;
+        long p;
+
+        p = (seed2 ^ (seed & 0xFFFFFFFFL)) * (seed1 ^ (seed >>> 32));
+        seed1 = (int) p;
+        seed2 = (int) (p >>> 32);
+        p = (seed3 ^ (seed & 0xFFFFFFFFL)) * (seed4 ^ (seed >>> 32));
+        seed3 = (int) p;
+        seed4 = (int) (p >>> 32);
+
+        int i = start;
+        if (len <= 4) {
+            if (len == 0) {
+                a = 0;
+                b = 0;
+            } else {
+                int last = i + len - 1;
+                a = data.charAt(i);
+                b = data.charAt(last);
+                if (len > 2) {
+                    int mo = len >>> 1;
+                    c = data.charAt(i + mo);
+                    d = data.charAt(last - mo);
+                    p = ((long) seed3 + c) * ((long) seed4 + d);
+                    seed3 = (int) p;
+                    seed4 = (int) (p >>> 32);
+                }
+            }
+        } else {
+            val01 ^= seed1;
+            val10 ^= seed2;
+
+            do {
+                final int s1 = seed1;
+                final int s4 = seed4;
+
+                p = ((long) seed1 + data.charAt(i)) * ((long) seed2 + data.charAt(i + 1));
+                seed1 = (int) p;
+                seed2 = (int) (p >>> 32);
+                p = ((long) seed3 + data.charAt(i + 2)) * ((long) seed4 + data.charAt(i + 3));
+                seed3 = (int) p;
+                seed4 = (int) (p >>> 32);
+
+                len -= 4;
+                i += 4;
+
+                seed1 += val01;
+                seed2 += s4;
+                seed3 += s1;
+                seed4 += val10;
+            } while (len > 4);
+
+            a = data.charAt(i + len - 2);
+            b = data.charAt(i + len - 1);
+
+            if (len > 2) {
+                c = data.charAt(i + len - 4);
+                d = data.charAt(i + len - 3);
+
+                p = ((long) seed3 + c) * ((long) seed4 + d);
+                seed3 = (int) p;
+                seed4 = (int) (p >>> 32);
+            }
+        }
+        seed1 ^= seed3;
+        seed2 ^= seed4;
+
+        p = ((long) seed1 + a) * ((long) seed2 + b);
+        seed1 = (int) p;
+        seed2 = (int) (p >>> 32);
+
+        p = ((long) seed1 ^ val01) * ((long) seed2);
+        a = (int) p;
+        b = (int) (p >>> 32);
+
+        return a ^ b;
+    }
 }
